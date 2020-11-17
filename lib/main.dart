6b75a7bcc70f17970c5fa197a3d412d52d49145b
FlutLab.io <>
2020-11-17 06:19:13 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<List<Mhs>> fetchMhss(http.Client client) async {
+Future<List<guru>> fetchMhss(http.Client client) async {
   final response =
       await client.get('https://ekifluter.000webhostapp.com/readDatajsonMid.php');
 
